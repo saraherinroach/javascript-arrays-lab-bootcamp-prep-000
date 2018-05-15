bbd913a5bf7abc1ae19650a61be4e5e72bd553d9
Sarah Erin Roach <saraherinroach@gmail.com>
2018-05-15 20:33:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ function destructivelyAppendKitten(name) {
   kittens.push(name);
   return kittens
 }
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens
+}
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+  return kittens
+}
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+  return kittens
+}
+function appendKitten(name) {
+  return [...kittens,name]
+}
